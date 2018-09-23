7c0833a8fda28cd2c06a2541def31db5a7efa8af
Vadim <bogomazov.vadim@yandex.ru>
2018-09-23 19:57:05 +0300

    Script update

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 export default () => {
   const div = document.createElement('div')
 
-  div.style.width = '100px'
-  div.style.height = '100px'
-  div.style.overflow = 'scroll'
-  div.style.position = 'absolute'
-  div.style.top = '-9999px'
+  div.style.cssText = "width: 100px; height: 100px; overflow: scroll; position: absolute; top: -9999px;"
 
   document.body.appendChild(div)
 
